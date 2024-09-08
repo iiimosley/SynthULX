9ef12b889e9645d2e4cc4e3ca4967ebbc6c72a9e
Trey Mosley <iiimosley@gmail.com>
2024-09-07 21:44:52 -0500

    feat: Set Builder State Store

--- v1/javascripts/edu-view.js
+++ v1/javascripts/edu-view.js
@@ -2,26 +2,15 @@
 const $ = require('jquery');
 const eduCtrl = require('./edu-control');
 const osc = require('../templates/osc.hbs');
-const oscDetail = require('../templates/osc-detail.hbs');
 const ampEG = require('../templates/amp-eg.hbs');
-const ampEGDetail = require('../templates/amp-eg-detail.hbs');
 const cutoffFilter = require('../templates/filter.hbs');
-const filterDetail = require('../templates/filter-detail.hbs');
 const filterEG = require('../templates/filter-eg.hbs');
-const filterEGDetail = require('../templates/filter-eg-detail.hbs');
 const buildComplete = require('../templates/complete.hbs');
 
 module.exports.startBuild = () => {
     $("#eduModal").empty();
     $("#eduModal").append(osc);
     $("#eduModal").show();
-};
-
-module.exports.printOscDetail = (wave) => {
-    let oscObj = {};
-    oscObj[wave] = true;
-    $("#oscDetail").empty();
-    $("#oscDetail").append(oscDetail(oscObj));
 };
 
 //if no oscillator selected: flashes screen to user prompt selection
@@ -38,37 +27,17 @@ module.exports.printAmpEG = () => {
     eduCtrl.ampDraw();
 };
 
-module.exports.printAmpEGDetail = (EG) => {
-    let ampObj = {};
-    ampObj[EG] = true;
-    $("#ampDetail").empty();
-    $("#ampDetail").append(ampEGDetail(ampObj));
-};
-
 module.exports.printFilter = () => {
     $("#eduWrap").empty();
     $("#eduWrap").append(cutoffFilter);
     eduCtrl.cutoffDraw();
 };
 
-module.exports.printFilterDetail = (f) => {
-    let fObj = {};
-    fObj[f] = true;
-    $("#cutoffDetail").empty();
-    $("#cutoffDetail").append(filterDetail(fObj));
-};
 
 module.exports.printFilterEG = () => {
     $("#eduWrap").empty();
     $("#eduWrap").append(filterEG);
     eduCtrl.filterDraw();
-};
-
-module.exports.printFilterEGDetail = (f) => {
-    let fObj = {};
-    fObj[f] = true;
-    $("#filterDetail").empty();
-    $("#filterDetail").append(filterEGDetail(fObj));
 };
 
 module.exports.leaveBuilder = () => {
